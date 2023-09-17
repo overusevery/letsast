35b86acaed0a7fa4d04345fe83a9d00d3893cbae
tomohiro.yamaoka <overusevery@gmail.com>
2023-09-17 18:32:07 +0900

    add option for  func listing

--- parser/main.go
+++ parser/main.go
@@ -16,36 +16,44 @@ func main() {
 		os.Exit(1)
 	}
 
-	listFunc(node)
+	//listFunc(node, listFuncOption{printInOutArgument: false})
+	listFunc(node, listFuncOption{printInOutArgument: true})
+
 	//for debug
 	//ast.Print(fset, node)
 }
 
-func listFunc(node *ast.File) {
+type listFuncOption struct {
+	printInOutArgument bool
+}
+
+func listFunc(node *ast.File, opt listFuncOption) {
 	ast.Inspect(node, func(n ast.Node) bool {
 		value, ok := n.(*ast.FuncDecl)
 		if ok {
 			fmt.Printf("Name:%v\n", value.Name.Name)
-			for _, param := range value.Type.Params.List {
-				switch paramType := param.Type.(type) {
-				case *ast.Ident:
-					for _, variable := range param.Names {
-						fmt.Printf(" - input:%v(%v)\n", variable.Name, paramType)
+			if opt.printInOutArgument {
+				for _, param := range value.Type.Params.List {
+					switch paramType := param.Type.(type) {
+					case *ast.Ident:
+						for _, variable := range param.Names {
+							fmt.Printf(" - input:%v(%v)\n", variable.Name, paramType)
+						}
+					case *ast.Ellipsis:
+						//example: (int...)
+						for _, variable := range param.Names {
+							ident, _ := paramType.Elt.(*ast.Ident)
+							fmt.Printf(" - input:%v(%v...)\n", variable.Name, ident.Name)
+						}
+					default:
+						panic("not implemnted for this type")
 					}
-				case *ast.Ellipsis:
-					//example: (int...)
-					for _, variable := range param.Names {
-						ident, _ := paramType.Elt.(*ast.Ident)
-						fmt.Printf(" - input:%v(%v...)\n", variable.Name, ident.Name)
-					}
-				default:
-					panic("not implemnted for this type")
-				}
 
-			}
-			if value.Type.Results != nil {
-				for _, output := range value.Type.Results.List {
-					fmt.Printf(" - output:(%v)\n", output.Type)
+				}
+				if value.Type.Results != nil {
+					for _, output := range value.Type.Results.List {
+						fmt.Printf(" - output:(%v)\n", output.Type)
+					}
 				}
 			}
 		}
