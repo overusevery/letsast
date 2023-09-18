5bb81ec87d8dac96cecddd0ba7a87104f5bea661
tomohiro.yamaoka <overusevery@gmail.com>
2023-09-18 14:33:56 +0900

    change read from file to dir

--- parser/main.go
+++ parser/main.go
@@ -10,20 +10,27 @@ import (
 
 func main() {
 	fset := token.NewFileSet()
-	node, err := parser.ParseFile(fset, "../example_program/main.go", nil, parser.AllErrors)
+	//node, err := parser.ParseFile(fset, "../example_program/main.go", nil, parser.AllErrors)
+	pkgs, err := parser.ParseDir(fset, "../example_program", nil, parser.AllErrors)
 	if err != nil {
 		fmt.Println("parse error:", err)
 		os.Exit(1)
 	}
-	//for debug
-	//ast.Print(fset, node)
+	for pkgName, pkg := range pkgs {
+		fmt.Printf("package:%v\n", pkgName)
+		for fileName, node := range pkg.Files {
+			fmt.Printf(" - file:%v\n", fileName)
+			//for debug
+			//ast.Print(fset, node)
 
-	//listFunc(node, listFuncOption{printInOutArgument: false})
-	fmt.Println("function")
-	listFunc(node, listFuncOption{printInOutArgument: true})
+			fmt.Println("function")
+			//listFunc(node, listFuncOption{printInOutArgument: false})
+			listFunc(node, listFuncOption{printInOutArgument: true})
 
-	fmt.Println("struct")
-	listStruct(node)
+			fmt.Println("struct")
+			listStruct(node)
+		}
+	}
 
 }
 
@@ -35,19 +42,19 @@ func listFunc(node *ast.File, opt listFuncOption) {
 	ast.Inspect(node, func(n ast.Node) bool {
 		value, ok := n.(*ast.FuncDecl)
 		if ok {
-			fmt.Printf("Name:%v\n", value.Name.Name)
+			fmt.Printf(" - Name:%v\n", value.Name.Name)
 			if opt.printInOutArgument {
 				for _, param := range value.Type.Params.List {
 					switch paramType := param.Type.(type) {
 					case *ast.Ident:
 						for _, variable := range param.Names {
-							fmt.Printf(" - input:%v(%v)\n", variable.Name, paramType)
+							fmt.Printf("   - input:%v(%v)\n", variable.Name, paramType)
 						}
 					case *ast.Ellipsis:
 						//example: (int...)
 						for _, variable := range param.Names {
 							ident, _ := paramType.Elt.(*ast.Ident)
-							fmt.Printf(" - input:%v(%v...)\n", variable.Name, ident.Name)
+							fmt.Printf("   - input:%v(%v...)\n", variable.Name, ident.Name)
 						}
 					default:
 						panic("not implemnted for this type")
@@ -56,7 +63,7 @@ func listFunc(node *ast.File, opt listFuncOption) {
 				}
 				if value.Type.Results != nil {
 					for _, output := range value.Type.Results.List {
-						fmt.Printf(" - output:(%v)\n", output.Type)
+						fmt.Printf("   - output:(%v)\n", output.Type)
 					}
 				}
 			}
@@ -71,12 +78,12 @@ func listStruct(node *ast.File) {
 		if value, ok := n.(*ast.GenDecl); ok && value.Tok == token.TYPE {
 			for _, spec := range value.Specs {
 				tspec, _ := spec.(*ast.TypeSpec)
-				fmt.Printf("Name:%v\n", tspec.Name.Name)
+				fmt.Printf(" - Name:%v\n", tspec.Name.Name)
 				stype, _ := tspec.Type.(*ast.StructType)
 				for _, field := range stype.Fields.List {
 					switch fieldType := field.Type.(type) {
 					case *ast.Ident:
-						fmt.Printf("- filed:%v(%v)\n", field.Names[0].Name, fieldType.Name)
+						fmt.Printf("   - filed:%v(%v)\n", field.Names[0].Name, fieldType.Name)
 					default:
 						panic("not implemnted for this type")
 					}
